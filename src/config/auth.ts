63814d8e63f9838a2bc2c7c00581efefd04f1c16
caiqueribeiro <caique.rodrigues2@fatec.sp.gov.br>
2020-05-06 09:56:10 -0300

    Autenticação com JWT e rota autenticada

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'a6f311a08ecd31cced4fe73b50cd9631',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouters from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouters);
+routes.use('/auth', sessionsRouter);
 
 export default routes;
