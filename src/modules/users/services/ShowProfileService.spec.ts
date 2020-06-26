250e5d9a0473ed6603b8fe878b3edf2d7233e95b
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2020-06-26 16:50:45 -0300

    Controller e teste de profile

---
+++ src/modules/users/services/ShowProfileService.spec.ts
@@ -0,0 +1,25 @@
+import FakeUsersRepository from '@modules/users/repositories/fake/FakeUsersRepository';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able to show the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Zezinho da meia noite',
+      email: 'midnightze@example.com',
+      password: 'vsk00134l10',
+    });
+
+    const profile = await showProfile.execute({ user_id: user.id });
+
+    expect(profile.name).toBe('Zezinho da meia noite');
+    expect(profile.email).toBe('jmidnightze@example.com');
+  }); // teste 1
+});
